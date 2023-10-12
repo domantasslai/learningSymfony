467b4802e1c1d8daa847c7c361c36fe45d15eb28
Domantas <domantas.slaiciunas@dielektrik.com>
2023-10-12 20:35:25 +0300

    .

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Michelf\MarkdownInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -28,16 +31,25 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="questions.show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdown, CacheInterface $cache)
     {
         $answers = [
-            'Make sure your cat is sitting perfectly still',
+            'Make sure your cat is sitting `perfectly` still',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
 
+        $questionText = "I've been turned into a cat, any *thoughts* on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function () use ($markdown, $questionText) {
+            return $markdown->transformMarkdown($questionText);
+        });
+
+        dump($cache);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
